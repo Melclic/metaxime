55bbe0b0346242d90f6834334d58a82ea27792e7
mdulac <melchior.dulac@inra.fr>
2020-04-05 02:32:48 +0200

    fixed issue with species group updates

--- rpSBML.py
+++ rpSBML.py
@@ -1064,7 +1064,7 @@ class rpSBML:
     # @param bilevel_obj Tuple of size 2 with the weights associated with the targetSink and GEM objective function
     #
     #def mergeModels(self, target_rpsbml, pathway_id='rp_pathway', fillOrphanSpecies=False, compartment_id='MNXC3'):
-    def mergeModels(self, target_rpsbml):#, fillOrphanSpecies=False, compartment_id='MNXC3'):
+    def mergeModels(self, target_rpsbml, species_group_id='central_species'):#, fillOrphanSpecies=False, compartment_id='MNXC3'):
         #target_rpsbml.model = target_document.getModel()
         #Find the ID's of the similar target_rpsbml.model species
         ################ MODEL FBC ########################
@@ -1362,9 +1362,22 @@ class rpSBML:
         self._checklibSBML(source_groups, 'fetching the source model groups')
         target_groups = target_rpsbml.model.getPlugin('groups')
         self._checklibSBML(target_groups, 'fetching the target model groups')
+        #TODO: this will overwrite two groups of the same id, need to change
         for group in source_groups.getListOfGroups():
+            #for all the species that need to be converted, replace the ones that are
+            #if the group is the species group, replace the ones detected from model_species_convert
+            if group.getId()==species_group_id:
+                for spe_conv in model_species_convert:
+                    foundIt = False
+                    for member in group.getListOfMembers():
+                        if member.getIdRef()==spe_conv:
+                            member.setIdRef(model_species_convert[spe_conv])
+                            foundIt = True
+                            break
+                    if not foundIt:
+                        self.logger.error('Could not find '+str(spe_conv)+' to replace with '+str(model_species_convert[spe_conv]))
             self._checklibSBML(target_groups.addGroup(group),
-                    'copying the source groups "rp_pathway" to the target groups')
+                    'copying the source groups to the target groups')
         ###### TITLES #####
         target_rpsbml.model.setId(target_rpsbml.model.getId()+'__'+self.model.getId())
         target_rpsbml.model.setName(target_rpsbml.model.getName()+' merged with '+self.model.getId())
