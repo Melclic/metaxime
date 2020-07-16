785f174a2cc3846d06cf36ff8dc1bd0d2986291e
mdulac <melchior.dulac@inra.fr>
2020-07-16 20:55:55 +0200

    added logging

--- rpCache.py
+++ rpCache.py
@@ -16,7 +16,8 @@ import json
 
 logging.basicConfig(
     #level=logging.DEBUG,
-    level=logging.WARNING,
+    #level=logging.WARNING,
+    level=logging.ERROR,
     format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
     datefmt='%d-%m-%Y %H:%M:%S',
 )
