e71dc3250c285c25a6af411bb0c656de06bd8b66
mdulac <melchior.dulac@inra.fr>
2020-11-20 18:17:09 +0100

    changed the default to fraction

--- draft.py
+++ draft.py
@@ -336,11 +336,13 @@ groups_plugin = model.getPlugin('groups')
 
 
 
+
+###################### TEST ALL ############################
+
 from metaxime import rpCache
 import os
 rpcache = rpCache()
 rpcache.populateCache()
-
 
 from metaxime import rpReader
 rpReader.rp2ToCollection(os.path.join('/home/mdulac/workspace/melclic/metaxime/metaxime/test', 'data', 'rpreader', 'rp_pathways.csv'),
@@ -349,16 +351,33 @@ rpReader.rp2ToCollection(os.path.join('/home/mdulac/workspace/melclic/metaxime/m
                          os.path.join('/home/mdulac/workspace/melclic/metaxime/metaxime/test', 'data', 'rpreader', 'test.rpcol'),
                          rpcache=rpcache)
 
-
 from metaxime import rpFBA
 rpFBA.runCollection(os.path.join('/home/mdulac/workspace/melclic/metaxime/metaxime/test', 'data', 'rpreader', 'test.rpcol'),
                     os.path.join('/home/mdulac/workspace/melclic/metaxime/metaxime/test', 'data', 'rpfba', 'gem.xml'),
                     os.path.join('/home/mdulac/workspace/melclic/metaxime/metaxime/test', 'data', 'rpfba', 'test.rpcol'),
-                    num_workers=5,
-                    rpcache=rpcache)
+                    num_workers=1,
+                    rpcache=rpcache,
+                    )
 
+from metaxime import rpEquilibrator
+import os
+rpcache = None
+rpEquilibrator.runCollection(os.path.join('/home/mdulac/workspace/melclic/metaxime/metaxime/test', 'data', 'rpfba', 'test.rpcol'),
+                    		 os.path.join('/home/mdulac/workspace/melclic/metaxime/metaxime/test', 'data', 'rpequilibrator', 'test.rpcol'),
+                    		 rpcache=rpcache)
 
-hqn_c__64__MNXC3
+from metaxime import rpSelenzyme
+import os
+rpcache = None
+rpSelenzyme.runCollection(os.path.join('/home/mdulac/workspace/melclic/metaxime/metaxime/test', 'data', 'rpfba', 'test.rpcol'),
+						  83333,
+						  os.path.join('/home/mdulac/workspace/melclic/metaxime/metaxime/test', 'data', 'rpselenzyme', 'test.rpcol'),
+						  rpcache=rpcache)
+
+from metaxime import rpGlobalScore
+rpGlobalScore.runCollection(os.path.join('/home/mdulac/workspace/melclic/metaxime/metaxime/test', 'data', 'rpselenzyme', 'test.rpcol'),
+							os.path.join('/home/mdulac/workspace/melclic/metaxime/metaxime/test', 'data', 'rpglobalscore', 'test.rpcol',
+							rpcache=rpcache))
 
 from metaxime import rpMerge
 rpMerge.mergeSBMLFiles('/home/mdulac/workspace/melclic/metaxime/metaxime/test/data/rpfba/rpsbml_collection/models/rp_129_6_rpsbml.xml',
